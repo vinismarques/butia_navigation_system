64875f9a0ee136d2198c8d5a087a392db62cdea1
Vinicius Marques <vinicius_marques@live.com>
2018-04-18 23:32:46 -0300

    Launch para slam criado e otimização do amcl para evitar que o robô se perca ao girar.

---
+++ description/param/catkin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vinicius/at_home_ws/src/pioneer-3at/description/param/catkin/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/vinicius/at_home_ws/src/pioneer-3at/description/param/catkin/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vinicius/at_home_ws/src/pioneer-3at/description/param/catkin/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/vinicius/at_home_ws/src/pioneer-3at/description/param/catkin/src:$ROS_PACKAGE_PATH"
---
+++ description/param/catkin/build/gtest/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/vinicius/at_home_ws/src/pioneer-3at/description/param/catkin/build/gtest/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
